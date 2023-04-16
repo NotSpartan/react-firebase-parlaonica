c9084a08bab8d43d02d88b295bf996e53c1fba01
NotSpartan <josip.batinic9@gmail.com>
2023-04-16 15:07:42 +0200

    implementirane .env konstante aplikacije

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase confprocess.env.
+const firebaseConfig = {
+  apiKey: process.env._API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseUrl:process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
